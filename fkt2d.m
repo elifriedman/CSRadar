bec16efd2e11f0fc230e1246e8a1a057803a473e
Alexander Serrano <serrano.alexander94@gmail.com>
2016-02-18 11:57:09 -0500

    Update fkt2d.m
    changed A-> A_train
    
    test->A_test

--- fkt2d.m
+++ fkt2d.m
@@ -7,15 +7,15 @@ close all
 ms = rand(D,C);
 vars = zeros(D,D,C);
 
-A = zeros(D,N/2,C);
-test = zeros(D,N/2,C);
+A_train = zeros(D,N/2,C);
+A_test = zeros(D,N/2,C);
 S = zeros(D,D,C);
 for i = 1:C
     vs = rand(D,D)*2-1;
     vars(:,:,i) = vs*vs';
-    A(:,:,i) = mvnrnd(ms(:,i),vars(:,:,i),N/2)';
-    test(:,:,i) = mvnrnd(ms(:,i),vars(:,:,i),N/2)';
-    S(:,:,i) = A(:,:,i)*A(:,:,i)';
+    A_train(:,:,i) = mvnrnd(ms(:,i),vars(:,:,i),N/2)';
+    A_test(:,:,i) = mvnrnd(ms(:,i),vars(:,:,i),N/2)';
+    S(:,:,i) = A_train(:,:,i)*A_train(:,:,i)';
 end
 
 St = S(:,:,1)+S(:,:,2);
@@ -30,17 +30,17 @@ proj1 = q1(:,1);
 proj2 = q1(:,2);
 
 
-subsp1 = proj1*proj1'*test(:,:,1);
-subsp2 = proj1*proj1'*test(:,:,2);
+subsp1 = proj1*proj1'*A_test(:,:,1);
+subsp2 = proj1*proj1'*A_test(:,:,2);
 
-plot(test(1,:,1),test(2,:,1),'ro',test(1,:,2),test(2,:,2),'go')
+plot(test(1,:,1),test(2,:,1),'ro',A_test(1,:,2),A_test(2,:,2),'go')
 title('original test data')
 figure
 plot(subsp1(1,:),subsp1(2,:),'ro',subsp2(1,:),subsp2(2,:),'go')
 title('projected test data')
 
-subsp12 = proj2*proj2'*test(:,:,1);
-subsp22 = proj2*proj2'*test(:,:,2);
+subsp12 = proj2*proj2'*A_test(:,:,1);
+subsp22 = proj2*proj2'*A_test(:,:,2);
 figure
 plot(subsp12(1,:),subsp12(2,:),'ro',subsp22(1,:),subsp22(2,:),'go')
 title('projected test data (lambda_2)')
