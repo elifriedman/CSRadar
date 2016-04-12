e570b7a7770004a5589851ffb750ac28d6585e3d
Alexander Serrano <serrano.alexander94@gmail.com>
2016-04-12 16:35:02 -0400

    Added files via upload
    test knn with original features

--- experiments.m
+++ experiments.m
@@ -1,17 +1,17 @@
-
 %% EXPERIMENT  - CLASSIF. ON ORIGINAL FEATURE SPACE     X_original , 
+clc; clear all; close all ; 
 Range_profile = readdata('Rotation_1.csv'); % M by 2048
 %noise
 Range_profile = Range_profile.' ;
-
 %%
-var = 0.5
+var = 0.05
 
-Range_profile_n =  sqrt(var)*randn(0.5*size(Range_profile,1),size(Range_profile,2)) ;
+Range_profile_n =  0.05+ sqrt(var)*randn(0.5*size(Range_profile,1),size(Range_profile,2)) ;
 
 
 X_original = [Range_profile((1:0.5*size(Range_profile,1)),:) ; Range_profile_n ] ;   %2M by N 
 
+X_original = abs(X_original);
 
 T = [ ones( size(Range_profile_n,1) ,1) ; zeros( size(Range_profile_n,1) ,1) ] ;
 
@@ -22,14 +22,13 @@ mdl = fitcknn(X_original,T,'NumNeighbors',10) ;
 %testing
 RP_test = Range_profile((0.5*size(Range_profile,1)+1:end),:) ;  
 
-Noise_test = sqrt(var)*randn(0.5*size(Range_profile,1),size(Range_profile,2)) ;
+Noise_test = 0.05 + sqrt(var)*randn(0.5*size(Range_profile,1),size(Range_profile,2)) ;
 
-X_test =  [RP_test ; Noise_test ] ; 
+X_test = abs( [RP_test ; Noise_test ]) ; 
 
-[label_test] = predict(mdl, X_test) ; 
+[label_test] = predict(mdl,X_test );
 
-score =  sum ( label_test == T ) / length(T) ; 
+score =  sum ( label_test == T ) / length(T) 
 
 
-%%
-
+%% MDA_FKT
