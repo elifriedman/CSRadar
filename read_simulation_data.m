c20fbbe96cfc3ce4cad50af6bf8ea9a77bb7dbbf
Alexander Serrano <serrano.alexander94@gmail.com>
2016-05-18 14:12:37 -0400

    Add files via upload

--- read_simulation_data.m
+++ read_simulation_data.m
@@ -1,11 +1,20 @@
-%clear workspace in Matlab 
 clear all
-%close all images
-%close all
-%clear screen
-clc
+close all;
+clc;
 %simulation data
-f = openfig('20degree_clockwise.fig');
+
+%strings 
+%{
+6 DIFFERENT ROTATIONS (FOR EACH OF THE 4 POSITIONS)
+20degree_Back
+20degree_Back20degreeClockwise
+20degree_Back20degreeCounterClockwise
+20degree_clockwise
+20degree_counterclockwise
+Norotation
+%}
+str =  '20degree_Back20degreeCounterClockwise'
+f = openfig(strcat(str,'.fig'));
 Ap  = 0.01;
 H = findobj(f, 'type','line');
 x_data = get(H, 'xdata');
@@ -22,11 +31,13 @@ y_data = get(H, 'ydata');
 %y=-30cm
 x_data_1 = x_data{2};
 y_data_1 = y_data{2};
-figure;
+f2 = figure;
 plot(x_data_1, y_data_1,'r','LineWidth',2)
 xlabel('range/m','linewidth',12,'fontsize',10,'Fontname','Timesnewroman','fontWeight','bold');
 ylabel('amplitude','linewidth',12,'fontsize',10,'Fontname','Timesnewroman','fontWeight','bold')
-title('Range Profile 1st position 0 degree')
+title(['Range Profile 1st position,',' ', str]);
+
+saveas(f2, ['Range Profile 1st position,',' ', str,'.png']);
 % xlim([0 3])
 
 
