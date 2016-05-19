005f64ebee70e391114ecd31b31198928b5eb762
elifriedman <eliyahufriedman@gmail.com>
2016-05-19 11:17:49 -0400

    edited freq_clustering

--- freq_clustering.m
+++ freq_clustering.m
@@ -1,13 +1,11 @@
-d = 101;
-N = 24;
-X = zeros(N,d);
-prefix = 'fwd_csradar_mat_files/Voltage_new_case';
-for i = 1:24
-    s = strcat(prefix,int2str(i),'.mat');
-    load(s)
-    X(i,:) = transpose(x);
-end
+%load frequency_profiles
+load range_profiles
+
+N = size(X,1);
+d = size(X,2);
 
 X = abs(X);
 X = X - ones(N,1)*mean(X,1);
 X = X ./ (ones(N,1)*std(X,1));
+
+idx = kmeans(X,6,'Replicates',9)'
