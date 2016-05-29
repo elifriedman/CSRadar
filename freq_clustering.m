bc11f554dc4a9e3708a0d9753c4d8de38c19b61f
Alexander Serrano Professional <Alexander@Alexanders-MacBook-Pro-3.local>
2016-05-29 10:00:58 -0400

    iterted through different k values

--- freq_clustering.m
+++ freq_clustering.m
@@ -1,5 +1,21 @@
-%load frequency_profiles
+%load range profile or frequency data
+clc; clear all; close all; 
+
+%% run this section or skip to the next
 load range_profiles
+
+%% 
+
+%{
+load frequency_profiles
+
+
+reduce_factor = 0.5
+X = rand_subsample(X,floor(size(X,2)*reduce_factor));
+
+%}
+
+%%
 
 N = size(X,1);
 d = size(X,2);
@@ -8,4 +24,25 @@ X = abs(X);
 X = X - ones(N,1)*mean(X,1);
 X = X ./ (ones(N,1)*std(X,1));
 
-idx = kmeans(X,6,'Replicates',9)'
+
+for k = 2:1:10 
+    
+idx = kmeans(X,k,'Replicates',9);
+
+clustered_mat = reshape(idx,[6 4 ] )
+
+%{
+6 angular orientations x 4 locations
+
+ANGLES:
+1 - No rotation
+2 - 20 degree ( counter-clockwise)
+3 - 20 degree (clockwise)
+4 - Back 
+5 - Back  20 degree(counter-clockwise)
+6 - Back 20 (clockwise)
+
+%}
+
+
+end
